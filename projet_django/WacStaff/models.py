bcce13aaa4dc20425406b0eb145c83ad88fed5b5
Kolwaan <nicolas-colin@outlook.fr>
2026-03-09 13:46:43 +0100

    créations des modèles + migrations

--- projet_django/WacStaff/models.py
+++ projet_django/WacStaff/models.py
@@ -2,34 +2,44 @@ from django.db import models
 
 
 class Collaborateur(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    nom = models.CharField(max_length=100, unique=True)
     prenom = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     date1ereEmbauche = models.DateTimeField(auto_now_add=True)
-    admin = models.BooleanField
-    password = models.CharField(max_length=200, unique=True) | None
+    admin = models.BooleanField(default=False)
+    password = models.CharField(max_length=200, blank=True, null=True)    
     
-
     def __str__(self):
-        return self.name
+        return self.nom
 
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
+class Restaurant(models.Model):
+    nom = models.CharField(max_length=100, unique=True)
+    adresse = models.CharField(max_length=300, unique=True)
+    codePostal = models.CharField(max_length=50)
+    ville = models.CharField(max_length=100)
+    
     def __str__(self):
-        return self.name
+        return self.nom
 
 
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField(Tag, related_name='posts')
-
+class Fonction(models.Model):
+    intitulePoste = models.CharField(max_length=100, unique=True)
+    
     def __str__(self):
-        return self.title
+        return self.intitulePoste
+
+
+
+class Affectation(models.Model):
+    collaborateur = models.ForeignKey(Collaborateur, on_delete=models.PROTECT, related_name='affectations') # One-to-Many
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT, related_name='affectations')   # One-to-Many
+    poste = models.ForeignKey(Fonction, on_delete=models.PROTECT, related_name='affectations')  # One-to-Many
+    # Avec Django, related_name évite d'ajouter une relation dans les autres classes 
+    debut = models.DateTimeField(auto_now_add=True) # auto_now_add remplit automatiquement avec la date du jour à la création
+    fin = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.collaborateur} — {self.restaurant}"
